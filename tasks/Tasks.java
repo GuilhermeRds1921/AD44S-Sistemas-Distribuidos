865611e54412bb7e7a55bd4c037068c025d91237
GuilhermeRds1921 <guilhermerodrigues1921@gmail.com>
2025-09-24 21:56:11 -0300

    adiciona classe Tasks com cálculo de soma e tempo de execução

---
+++ tasks/Tasks.java
@@ -0,0 +1,21 @@
+public class Tasks {
+
+    public static final int IT = 100;
+    public static final long LIMIT = 100000000;
+
+    public static void main(String[] args) {
+        double sum = 0, start, end;
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i <= IT; i++) {
+            sum += 0;
+            for (long j = 0; j <= LIMIT; j++) {
+                sum += j;
+            }
+        }
+        end = System.currentTimeMillis();
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("Execution time: " + (end - start) + " ms");
+    }
+}
